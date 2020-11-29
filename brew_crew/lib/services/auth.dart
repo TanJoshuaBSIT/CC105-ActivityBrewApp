6b9f35961d93699e95420df64fa8b8dceded52f0
unknown <bg201802398@wmsu.edu.ph>
2020-11-29 20:32:07 +0800

    NetNinja Tutorial using Firebase and Flutter

---
+++ brew_crew/lib/services/auth.dart
@@ -0,0 +1,69 @@
+import 'package:brew_crew/models/user.dart';
+import 'package:brew_crew/services/database.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //make user obj based on firebaseUser
+  User _userFromFirebaseUser(FirebaseUser user) {
+    return user != null ? User(uid: user.uid) : null;
+  }
+
+  //auth change user stream
+  Stream<User> get user {
+    return _auth.onAuthStateChanged.map(_userFromFirebaseUser);
+  }
+
+//sign anon
+  Future signInAnon() async {
+    try {
+      AuthResult result = await _auth.signInAnonymously();
+      FirebaseUser user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+//sign email and pass
+  Future signInWithEmailAndPass(String email, String password) async {
+    try {
+      AuthResult result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      FirebaseUser user = result.user;
+
+      //create new doc with uid
+      await DatabaseService(uid: user.uid)
+          .updateUserData('0', 'new crew dude', 100);
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+//reg with email and pass
+  Future registerWithEmailAndPass(String email, String password) async {
+    try {
+      AuthResult result = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      FirebaseUser user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+//sign out
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+}
---
+++ brew_crew/lib/services/database.dart
@@ -0,0 +1,50 @@
+import 'package:brew_crew/models/brew.dart';
+import 'package:brew_crew/models/user.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DatabaseService {
+  final String uid;
+  DatabaseService({this.uid});
+//collection ref
+  final CollectionReference brewCollection =
+      Firestore.instance.collection('brews');
+
+  Future updateUserData(String sugars, String name, int strength) async {
+    return await brewCollection.document(uid).setData({
+      'sugars': sugars,
+      'name': name,
+      'strength': strength,
+    });
+  }
+
+  //brewlist from snap
+  List<Brew> _brewListFromSnap(QuerySnapshot snapshot) {
+    return snapshot.documents.map((doc) {
+      return Brew(
+        name: doc.data['name'] ?? '',
+        strength: doc.data['strength'] ?? 0,
+        sugars: doc.data['sugars'] ?? '0',
+      );
+    }).toList();
+  }
+
+  //user data from snap
+  UserData _userDataFromSnap(DocumentSnapshot snapshot) {
+    return UserData(
+      uid: uid,
+      name: snapshot.data['name'],
+      sugars: snapshot.data['sugars'],
+      strength: snapshot.data['strength'],
+    );
+  }
+
+  //get strem
+  Stream<List<Brew>> get brews {
+    return brewCollection.snapshots().map(_brewListFromSnap);
+  }
+
+  //get user doc stream
+  Stream<UserData> get userData {
+    return brewCollection.document(uid).snapshots().map(_userDataFromSnap);
+  }
+}
